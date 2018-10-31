54d63482f015c379f834321df2dd552a6f8a2f70
Jean-Michel Fayard <jmfayard@gmail.com>
2018-10-31 14:28:32 +0100

    Extract variables to buildSrc/src/main/kotlin/Config.kt

---
+++ buildSrc/src/main/kotlin/Config.kt
@@ -0,0 +1,8 @@
+object Config {
+    const val applicationId = "com.wealthfront.magellan.kotlinsample"
+    const val compileSdkVersion = 27
+    const val targetSdkVersion = 27
+    const val versionName = "1.0.0"
+    const val versionCode = 1
+    const val minSdkVersion = 21
+}
